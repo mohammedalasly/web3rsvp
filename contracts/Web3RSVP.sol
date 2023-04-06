522d40f59f0f0698c247a71031535f5ceea0740d
Mohammad Alasli <mohammedalasly5@gmail.com>
2023-04-06 14:39:18 +0300

    create deploy file in the scripts folder to deploy our contract

--- contracts/Web3RSVP.sol
+++ contracts/Web3RSVP.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.17;
+pragma solidity ^0.8.18;
 
 ///@title RSVP Dapp.
 // a web3-native Eventbrite, except attendees need to deposit ETH to RSVP
